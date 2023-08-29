b1dc70a3c2b35d506c3c8e8f988421a7c4ad6781
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-08-29 22:49:58 +0700

    inference

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from tqdm import tqdm
+from streamlit_drawable_canvas import st_canvas
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential, load_model
@@ -71,29 +72,47 @@ def training(X, y):
             st.success(f'Done. Accuracy on test set: {round(accuracy,2)}')
 
 def inference(labels, input_shape):
-    uploaded_file = st.file_uploader('Upload Image File', type=['png','jpg','bmp'])
-    if uploaded_file is not None:
+    col1, col2 = st.columns(2)
+    with col1:
+        canvas_result = st_canvas(
+            stroke_width=8,
+            stroke_color='rgb(255, 255, 255)',
+            background_color='rgb(0, 0, 0)',
+            height=150,
+            width=150,
+            key="canvas",
+        )
+    if canvas_result.image_data is not None:img = Image.fromarray(canvas_result.image_data)
+
+    with col2:
+        uploaded_file = st.file_uploader('Upload Image File', type=['png','jpg','bmp'])
+        if uploaded_file is not None:img = Image.open(uploaded_file)
+
+    if st.button('Predict'):
         model = load_model('model.h5')
-        img = Image.open(uploaded_file)
-        st.text('Original Image')
-        st.image(img)
         img = img.resize(input_shape)
         img = img.convert('L')
         img = np.array(img, dtype=float)/255
-        col1, col2 = st.columns(2)
-        with col1:
-            st.text('Grayscale Image')
-            inference_image(model, labels, img, input_shape)
-        with col2:
-            st.text('Invert Grayscale Image')
-            inference_image(model, labels, 1-img, input_shape)
-            
-def inference_image(model, labels, img, input_shape):
-    label = model.predict(img.reshape(-1,*input_shape)).squeeze()
-    ids = np.argsort(label)[::-1]
-    st.image(img)
+        if uploaded_file is not None: 
+            col3, col4, col5 = st.columns(3)
+            with col3:
+                st.text('Original Image')
+                st.image(Image.open(uploaded_file))
+            with col4:
+                st.text('Grayscale Image')
+                inference_image(model, labels, img, input_shape)
+            with col5:
+                st.text('Invert Grayscale Image')
+                inference_image(model, labels, 1-img, input_shape)
+        else:
+            inference_image(model, labels, img, input_shape, False)
+
+def inference_image(model, labels, img, input_shape, draw_img=True):
+    y_pred = model.predict(img.reshape(-1,*input_shape)).squeeze()*100
+    ids = np.argsort(y_pred)[::-1]
+    if draw_img:st.image(img)
     for i in ids[:5]:
-        st.write(labels[i], label[i])
+        st.write(labels[i], y_pred[i].round(decimals=2))
 
 def main():
     st.title('HANDWRITING RECOGNITION')
